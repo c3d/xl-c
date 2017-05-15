63310695383da62d08ecc114db9e8569b942a90e
Christophe de Dinechin <dinechin@redhat.com>
2017-05-15 06:22:12 +0200

    Add BLOB_TYPEDEF for typed blobs

--- blob.h
+++ blob.h
@@ -36,12 +36,12 @@ typedef struct blob
 } blob_t;
 
 
-inline blob_p      blob_new(unsigned position, size_t sz, const char *data);
-inline void        blob_delete(blob_p blob);
-extern blob_p      blob_append(blob_p blob, size_t sz, const char *data);
-extern blob_p      blob_range(blob_p blob, size_t start, size_t len);
-inline const char *blob_data(blob_p blob);
-inline size_t      blob_size(blob_p blob);
+inline blob_p  blob_new(unsigned position, size_t sz, const char *data);
+inline void    blob_delete(blob_p blob);
+extern blob_p  blob_append(blob_p blob, size_t sz, const char *data);
+extern blob_p  blob_range(blob_p blob, size_t start, size_t len);
+inline char   *blob_data(blob_p blob);
+inline size_t  blob_size(blob_p blob);
 
 // Private blob handler, should not be called directly in general
 inline blob_p blob_make(tree_handler_fn h, unsigned pos, size_t, const char *);
@@ -83,12 +83,12 @@ inline void blob_delete(blob_p blob)
 }
 
 
-inline const char *blob_data(blob_p blob)
+inline char *blob_data(blob_p blob)
 // ----------------------------------------------------------------------------
 //   Return the data for the blob
 // ----------------------------------------------------------------------------
 {
-    return (const char *) (blob + 1);
+    return (char *) (blob + 1);
 }
 
 
@@ -100,5 +100,52 @@ inline size_t blob_size(blob_p blob)
     return blob->size;
 }
 
+
+
+// ============================================================================
+//
+//    Blobs for a given item type
+//
+// ============================================================================
+
+#define BLOB_TYPEDEF(item, name)                                        \
+typedef struct name *name##_p;                                          \
+                                                                        \
+inline name##_p name##_new(unsigned pos, size_t sz, const item *data)   \
+{                                                                       \
+    sz *= sizeof(item);                                                 \
+    const char *chardata = (const char *) data;                         \
+    return (name##_p)blob_new(pos, sz, chardata);                       \
+}                                                                       \
+                                                                        \
+inline void name##_delete(name##_p name)                                \
+{                                                                       \
+    blob_delete((blob_p) name)                                          \
+}                                                                       \
+                                                                        \
+inline name##_p name##_append(name##_p name, size_t sz, const item *data) \
+{                                                                       \
+    sz *= sizeof(item);                                                 \
+    const char *chardata = (const char *) data;                         \
+    return (name##_p) blob_append((blob_p) name, sz, chardata);         \
+}                                                                       \
+                                                                        \
+                                                                        \
+inline name##_p name##_range(name##_p name, size_t start, size_t len)   \
+{                                                                       \
+    start *= sizeof(item);                                              \
+    len *= sizeof(item);                                                \
+    return (name##_p) blob_range((blob_p) name, start, len);            \
+}                                                                       \
+                                                                        \
+inline item *name##_data(name##_p name)                                 \
+{                                                                       \
+    return (item *) blob_data((blob_p) name);                           \
+}                                                                       \
+                                                                        \
+inline size_t name##_length(blob_p blob)                                \
+{                                                                       \
+    return blob_size((blob_p) name) / sizeof(item);                     \
+}
 
 #endif // BLOB_H
